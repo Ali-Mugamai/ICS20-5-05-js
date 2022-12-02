34de61ed7684dcd33256bb9fe71a032624327d87
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-12-02 13:54:17 -0500

    gui

--- js/script.js
+++ js/script.js
@@ -45,5 +45,5 @@ function calculate() {
     Number(angleB.toFixed(2)) +
     Number(angleC.toFixed(2))
   // output
-  document.getElementById("pay").innerHTML = "Your pay will be:" + sumOfAngles
+  document.getElementById("pay").innerHTML = "<p> Your pay will be:" + sumOfAngles + "</p>"
 }
